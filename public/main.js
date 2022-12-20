719e33495bc767a38eb0971d2af4e947677e35df
faadhi <75142042+oxgr@users.noreply.github.com>
2022-12-20 04:40:48 -0500

    cleanup

--- public/main.js
+++ public/main.js
@@ -5,24 +5,13 @@ import Gui from './gui.js'
 
 await main()
 
-async function sleep( ms = 1000 ) {
-    return new Promise( r => setTimeout( r, ms ) )
-}
-
 async function main() {
 
     const mainView = new p5( Pong.run, document.getElementById( 'sketch' ) )
     const gridView = new p5( GridView, document.getElementById( 'view' ) )
 
-    // for ( const [key,val] of Object.entries( mainView ) ) {
-    //     // if ( val instanceof Function && val.toString().includes( 'SketchModel' )) console.log( val.toString() )
-    //     console.log( val )
-    // }
-    console.log( mainView._start )
-
-    // Wait a minute while p5 setup() instantiates model variables
+    // Wait 0.1 seconds while p5 setup() instantiates model variables
     await sleep(100)
-    // console.log( Pong.model )
 
     const gui = new Gui( {
         'Pong': {
@@ -35,23 +24,8 @@ async function main() {
         }
     }) 
 
+}
 
-    // const pong = new Pong()
-
-    // const MODE = 
-    //     'instance'
-    //     // 'global'
-
-    // switch ( MODE ) {
-
-    //     case 'instance':
-    //         new p5( pong.instanceTest(), document.getElementById( 'canvas' ) )
-    //         break
-
-    //     case 'global':
-    //         pong.global()
-    //         break
-
-    // }
-
+async function sleep( ms = 1000 ) {
+    return new Promise( r => setTimeout( r, ms ) )
 }
